ba5c593f4af89d545af505d9c2986fa3a632df7e
Krishna <nayak100krishna@gmail.com>
2022-09-11 11:00:30 +0530

    initail state change

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,10 +1,10 @@
 import { ActionTypes } from "../contants/action-types";
 
-const initail = [{ id: 1, title: "First Product", category: "Category 1" }];
+const initail = { products: [] };
 export const productReducer = (state = initail, action) => {
   switch (action.type) {
     case ActionTypes.SET_PRODUCTS:
-      return state;
+      return { ...state, products: action.payload };
     default:
       return state;
   }
