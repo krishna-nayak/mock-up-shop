809fb3cc7a5e29239e36025d6caee427dd8c77c0
Krishna <nayak100krishna@gmail.com>
2022-09-11 15:11:53 +0530

    reface-code product

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import axios from "axios";
 
 import { setProducts } from "../redux/actions/productActions.js";
@@ -9,7 +9,6 @@ import Container from "react-bootstrap/Container";
 import ProductComponent from "./ProductComponent";
 
 const ProductListing = () => {
-  const products = useSelector((state) => state);
   const dispatch = useDispatch();
 
   useEffect(() => {
